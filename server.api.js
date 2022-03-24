dd6e28d188f7259b9c6ded6976633d461ae72675
unknown <aarab.mohamed90@gmail.com>
2022-03-24 17:54:17 +0000

    white list heruko route

--- server.api.js
+++ server.api.js
@@ -5,7 +5,7 @@ const  cors = require('cors')
 const app = require('./app');
 
 
-const whitelist = ['http://localhost:7000','http://localhost:3000',]
+const whitelist = ['https://greetingslamp-api.herokuapp.com','http://localhost:7000','http://localhost:3000',]
 const corsOptions = {
     origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1) {
