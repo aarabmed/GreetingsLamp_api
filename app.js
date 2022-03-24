c75a8304924f754df6540780c2f445da2de47915
unknown <aarab.mohamed90@gmail.com>
2022-03-24 21:10:56 +0000

    allow admin origin

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-const whitelist = ['http://localhost:7000',]
+const whitelist = ['http://localhost:7000','https://greetingslamp-admin.herokuapp.com']
 const corsOptions = {
     origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -32,22 +32,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-/* 
-app.use(cors(corsOptions),(req, res, next) => {
-      
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
 
-   // console.log("Req:",req)
-});  */
 
 app.use("/account",accountRoute);
 app.use("/users",userRoutes);
